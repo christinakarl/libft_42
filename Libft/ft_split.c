0b8ef98efa85df0f465d140a2a743f912f1a92de
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2022-11-04 17:29:21 +0100

    split

--- Libft/ft_split.c
+++ Libft/ft_split.c
@@ -6,36 +6,101 @@
 /*   By: ckarl <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 13:19:20 by ckarl             #+#    #+#             */
-/*   Updated: 2022/11/02 18:35:50 by ckarl            ###   ########.fr       */
+/*   Updated: 2022/11/04 17:26:23 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-int words(char *s, char c)
+int	ft_len(const char *str)
 {
-    int i;
-    int count;
+	int	i;
 
-    i = 0;
-    count = 1;
-    while (s[i] != '\0')
-    {
-        if (s[i] == c)
-            count += 1;
-            
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
 
-    }
-
-}  
-
-char    **ft_split(char const *s, char c)
+int	ft_words(char const *s, char c)
 {
-    char    *str;
-    char    **tab;
-    int     i;
-    int     indicator;
+	int	i;
+	int	count;
+	int	point;
 
+	point = 1;
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && point == 1)
+		{
+			count += 1;
+			point = 0;
+		}
+		else if (s[i] == c)
+			point = 1;
+		i++;
+	}
+	return (count);
+}
 
+char	*ft_dup(char const *str, int start, int end)
+{
+	char	*copy;
+	int		i;
 
+	i = 0;
+	copy = (char *)malloc(sizeof(*copy) * (end - start + 1));
+	if (!copy)
+		return (NULL);
+	while (start < end)
+		copy[i++] = str[start++];
+	copy[i] = '\0';
+	return (copy);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**tab;
+	int		i;
+	int		a;
+	int		start;
+
+	i = 0;
+	a = 0;
+	start = -1;
+	tab = (char **)malloc(sizeof(char *) * (ft_words(s, c) + 1));
+	if (!tab || !s)
+		return (NULL);
+	while (i <= ft_len(s))
+	{
+		if (s[i] != c && start < 0)
+			start = i;
+		else if ((s[i] == c || i == ft_len(s)) && start >= 0)
+		{
+			tab[a++] = ft_dup(s, start, i);
+			start = -1;
+		}
+		i++;
+	}
+	tab[a] = 0;
+	return (tab);
+}
+
+int	main(void)
+{
+	int	i = 0;
+	char **tab;
+	char	*s = "8asf888asdf834423";
+	char	c = '8';
+
+	tab = ft_split(s, c);
+	while (tab[i] != 0)
+	{
+		printf("%s\n", tab[i]);
+		i++;
+	}
+	return (0);
 }
