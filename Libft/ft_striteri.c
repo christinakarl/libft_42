3db702e8a0ae903c9e0955584bb79260a1552ecf
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2022-11-24 17:08:10 +0100

    new

--- Libft/ft_striteri.c
+++ Libft/ft_striteri.c
@@ -16,7 +16,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 
 	i = 0;
 	if (!s || !f)
-		return (0);
+		return;
 	while (s[i])
 	{
 		f(i, &s[i]);
--- Libft/ft_strmapi.c
+++ Libft/ft_strmapi.c
@@ -22,7 +22,10 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	if (!str || !f)
 		return (NULL);
 	while (s[i])
-		str[i++] = f(i, s[i]);
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
 	str[i] = '\0';
 	return (str);
 }
