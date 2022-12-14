964b360d53958652c8b928ca43e2407b673b8119
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2022-12-14 18:33:02 +0100

    printf

---
+++ printf/ft_printf.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckarl <marvin@42lausanne.ch>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/08 16:38:30 by ckarl             #+#    #+#             */
+/*   Updated: 2022/10/27 17:12:36 by ckarl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "libftprintf.h"
+#include <stdarg.h>
+
+imp	*ft_init(imp *put)
+{
+	put.len = 0;
+	return (put);
+}
+//set the structure imp's component to 0 (except of the args)
+
+int	ft_printf(const char *print, ...)
+{
+	imp		*put;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = 0;
+	va_start(args, print);
+
+	while (print[++i])
+	{
+		if (print[i] == '%')
+
+
+	}
+
+	//len always + write(...) because write returns an int
+
+	va_end(args);
+	return (len);
+}
