e2817f636bbde940450313873bea178fbb3f1549
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2023-03-24 10:54:27 +0100

    friday

---
+++ fractol/fractol.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 17:14:41 by ckarl             #+#    #+#             */
+/*   Updated: 2023/03/23 18:32:14 by ckarl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+# include "mlx/mlx.h"
+//# include "libft.h"
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+typedef struct	s_fractol
+{
+	void	*ptr;
+	void	*window;
+
+} t_fract;
+
+//
+typedef struct s_getdata
+{
+	void	*img_ptr;
+	char	*addr;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+} t_getdata;
+
+
+
+#endif
---
+++ fractol/test.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int main() {
+printf("salut\n");
+return (0); }
