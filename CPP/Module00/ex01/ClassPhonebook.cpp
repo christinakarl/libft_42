1c4c6023f491d13ad7732caa89ae8ed7b6bdee35
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2023-11-23 16:04:15 +0100

    ready

--- CPP/Module00/ex01/ClassPhonebook.cpp
+++ CPP/Module00/ex01/ClassPhonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 19:42:09 by ckarl             #+#    #+#             */
-/*   Updated: 2023/11/23 14:46:40 by ckarl            ###   ########.fr       */
+/*   Updated: 2023/11/23 16:04:09 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 Phonebook::Phonebook( void ) {
 
 	this->index = 0;
+	this->total = 0;
 	return ;
 }
 
@@ -25,12 +26,18 @@ Phonebook::~Phonebook( void ) {
 
 void	Phonebook::addCtc( Contact ctc )
 {
+	if (this->index > 7)
+	{
+		this->index = 0;
+	}
 	this->_Book[this->index] = ctc;
 	this->_Book[this->index].index = index;
 	std::cout << "\033[1;35m\nThis contact was successfully added under index ";
 	std::cout << this->index;
 	std::cout << "\033[0m\n\n";
-	if (this->index < 7)
+	if (this->total < 8)
+		this->total++;
+	if (this->index <= 7)
 		this->index++;
 }
 
@@ -41,7 +48,7 @@ int	Phonebook::getctc_info( int ctc_index )
 		std::cout << "\033[0;35mThis index is out of range or invalid\033[0m\n";
 		return 1;
 	}
-	else if (this->index > ctc_index)
+	else if (this->total > ctc_index)
 		this->_Book[ctc_index].display_ctc_long();
 	else
 	{
@@ -55,13 +62,13 @@ int	Phonebook::display_book( void )
 {
 	int	i;
 
-	if (this->index == 0)
+	if (this->total == 0)
 	{
 		std::cout << "\n\033[0;35mYour phonebook is still empty, add contacts first\033[0m\n\n";
 		return 1;
 	}
 	std::cout << "\n\033[1;35mBelow you may find a list of your contacts\033[0m\n\n";
-	for (i = 0; i < this->index; i++)
+	for (i = 0; i < this->total; i++)
 	{
 		this->_Book[i].display_ctc_short();
 	}
