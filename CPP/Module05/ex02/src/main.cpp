aa8bca6cc0bd96665e5734232373d00079593446
christinakarl <christina.karl@posteo.de>
2024-01-17 18:19:43 +0100

    main_ex02

--- CPP/Module05/ex02/src/main.cpp
+++ CPP/Module05/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:11:25 by ckarl             #+#    #+#             */
-/*   Updated: 2024/01/17 18:15:19 by ckarl            ###   ########.fr       */
+/*   Updated: 2024/01/17 18:19:07 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,25 @@ int	main(void)
 	try {
 		PresidentialPardonForm f1("JustSomeone");
 		RobotomyRequestForm f2("robotomizeMe");
+		ShrubberyCreationForm f3("treetreetreetreee");
 		AForm&		robots = f2;
 		AForm&		fun = f1;
-		Bureaucrat	horst("horst", 140);
+		AForm&		trees = f3;
+		Bureaucrat	horst("horst", 166);
 
 		std::cout << std::endl;
 		// horst.signForm(fun);
+		// horst.signForm(trees);
 		horst.signForm(robots);
 		// horst.executeForm(fun);
+		// horst.executeForm(trees);
 		horst.executeForm(robots);
 		// std::cout << fun << std::endl;
 		// std::cout << horst << std::endl;
 		std::cout << std::endl;
 	}
 	catch (std::exception &e) {
-		std::cerr << "Exception caught: " << e.what() << std::endl;
-
+		std::cerr << "\nException caught: " << e.what() << std::endl;
 	}
 
 
