1927c49cb0378332c9f5fef2da98833d70e04d8b
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2024-03-11 14:53:40 +0100

    mod07/ex00

---
+++ CPP/Module07/ex00/main.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 15:34:04 by ckarl             #+#    #+#             */
+/*   Updated: 2024/03/11 14:50:56 by ckarl            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main()
+{
+	int a = 2;
+	int b = 3;
+
+	std::cout << "before the swap: a = " << a << ", b = " << b << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	std::cout << "before the swap: c = " << c << ", d = " << d << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
